7f557712fdcae0c1544fa8ac41e1777a5d7e0ab5
Victor Novais <victornovais@Victors-Laptop.local>
2024-07-09 16:07:28 -0300

    feat/start-backend

---
+++ backend/lib/banana_bank/users/user.ex
@@ -0,0 +1,34 @@
+defmodule BananaBank.Users.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @required_params [:name, :password, :email, :cep]
+
+  alias Ecto.Changeset
+
+  schema "users" do
+    field :name, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+    field :email, :string
+    field :cep, :string
+
+    timestamps()
+  end
+
+  def changeset(user \\ %__MODULE__{}, params) do
+    user
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:name, min: 3)
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:cep, is: 8)
+    |> add_password_hash()
+  end
+
+  defp add_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Argon2.add_hash(password))
+  end
+
+  defp add_password_hash(changeset), do: changeset
+end
